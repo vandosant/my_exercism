761d089bed6a23fe33e82889244ed695cfed81bb
vandosant <sjskender@gmail.com>
2014-04-11 16:03:07 -0600

    Oops -- fixed folder setup

--- ruby/ruby/bob/bob_test.rb
+++ ruby/ruby/bob/bob_test.rb
@@ -29,12 +29,6 @@ class TeenagerTest < MiniTest::Unit::TestCase
     assert_equal 'Woah, chill out!', teenager.hey('WATCH OUT!')
   end
 
-  def test_shouting_gibberish
-    skip
-    gibberish = ('A'..'Z').to_a.shuffle[0,10].join
-    assert_equal 'Woah, chill out!', teenager.hey(gibberish)
-  end
-
   def test_asking_a_question
     skip
     assert_equal 'Sure.', teenager.hey('Does this cryogenic chamber make me look fat?')
@@ -43,12 +37,6 @@ class TeenagerTest < MiniTest::Unit::TestCase
   def test_asking_a_numeric_question
     skip
     assert_equal 'Sure.', teenager.hey('You are, what, like 15?')
-  end
-
-  def test_asking_gibberish
-    skip
-    gibberish = ('a'..'z').to_a.shuffle[0,10].join
-    assert_equal 'Sure.', teenager.hey("#{gibberish}?")
   end
 
   def test_talking_forcefully
@@ -108,8 +96,7 @@ class TeenagerTest < MiniTest::Unit::TestCase
 
   def test_prolonged_silence
     skip
-    silence = " " * rand(1..10)
-    assert_equal 'Fine. Be that way!', teenager.hey(silence)
+    assert_equal 'Fine. Be that way!', teenager.hey('    ')
   end
 
   def test_on_multiple_line_questions
--- ruby/ruby/strain/README.md
+++ ruby/ruby/strain/README.md
@@ -1,6 +1,8 @@
 # Strain
 
-Implement the `keep` and `discard` operation on collections. Given a collection and a predicate on the collection's elements, `keep` returns a new collection containing those elements where the predicate is true, while `discard` returns a new collection containing those elements where the predicate is false.
+Implement the `keep` and `discard` operation on collections. Given a collection and a predicate on the collection's elements,
+`keep` returns a new collection containing those elements where the predicate is true, while `discard` returns a new collection
+containing those elements where the predicate is false.
 
 For example, given the collection of numbers:
 
